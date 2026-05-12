66283bb47b0880f28de68601ec8da84386a51433
sanjana_2011 <sanjanagangrade20816@acropolis.in>
2026-05-12 15:38:34 +0530

    done

--- ORSProject-10-backend/src/main/java/com/rays/dao/EventDAOInt.java
+++
@@ -1,7 +0,0 @@
-package com.rays.dao;
-
-import com.rays.common.BaseDAOInt;
-import com.rays.dto.EventDTO;
-
-public interface EventDAOInt extends BaseDAOInt<EventDTO> {
-}
--- ORSProject-10-backend/src/main/java/com/rays/service/EventServiceInt.java
+++
@@ -1,7 +0,0 @@
-package com.rays.service;
-
-import com.rays.common.BaseServiceInt;
-import com.rays.dto.EventDTO;
-
-public interface EventServiceInt extends BaseServiceInt<EventDTO> {
-}
