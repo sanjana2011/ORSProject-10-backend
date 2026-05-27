d47e03674d6e4190b14b32cf73072774e9d6c9ab
Sanjana Gangrade <87362088+sanjana2011@users.noreply.github.com>
2026-05-27 12:11:03 +0530

    Update EventServiceInt.java

--- src/main/java/com/rays/service/EventServiceInt.java
+++ src/main/java/com/rays/service/EventServiceInt.java
@@ -3,5 +3,5 @@ package com.rays.service;
 import com.rays.common.BaseServiceInt;
 import com.rays.dto.EventDTO;
 
-public interface EventServiceInt extends BaseServiceInt<EventDTO> {
+public interface EventServiceInt extends BaseServiceInt<EventDTO> { 
 }
